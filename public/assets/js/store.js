3772f7055eea55623213c2beec217b5ae74418a0
aprudnikov <a.prudnikov@neusta.de>
2019-09-24 10:58:21 +0200

    weitere funktionen

--- public/assets/js/store.js
+++ public/assets/js/store.js
@@ -1,9 +1,13 @@
-
 (function($) {
     'use strict';
 
+    const storageKey = 'wishlist';
+    let wishList = loadWishList();
+
+    highlightWishItems();
+
     // Main Navigation
-    $( '.hamburger-menu' ).on( 'click', function() {
+    $('.hamburger-menu').on('click', function() {
         $(this).toggleClass('close');
         $('.site-branding').toggleClass('hide');
         $('.site-navigation').toggleClass('show');
@@ -11,7 +15,7 @@
     });
 
     // Scroll to Next Section
-    $( '.scroll-down' ).click(function() {
+    $('.scroll-down').click(function() {
         $( 'html, body' ).animate({
             scrollTop: $( '.scroll-down' ).offset().top + 100
         }, 800 );
@@ -27,6 +31,57 @@
         modal.find('.modal-body').html('<div class="spinner-box"><div class="spinner-wrapper"><div class="spinner"></div></div></div>');
         $.get(url, function(html){
             modal.find('.modal-body').html(html);
+            highlightWishItem(id);
         });
-    })
+    });
+
+    function highlightWishItems() {
+        for (let i = 0; i < wishList.length; i++) {
+            $('.btn_ProceedWishList[data-item-id='+wishList[i]+']').addClass('item-action-active');
+        }
+    }
+
+    function highlightWishItem(itemId) {
+        if($.inArray(itemId, wishList) !== -1) {
+            $('.btn_ProceedWishList[data-item-id='+itemId+']').addClass('item-action-active');
+        } else {
+            $('.btn_ProceedWishList[data-item-id='+itemId+']').removeClass('item-action-active');
+        }
+    }
+
+    $('body').on('click', '.btn_ProceedWishList', function(e) {
+        e.preventDefault();
+        let $this = $(this);
+        let itemId = $this.data('item-id');
+        if($.inArray(itemId, wishList) === -1) {
+            addToWishList(itemId);
+        } else {
+            removeFromWishList(itemId);
+        }
+        highlightWishItem(itemId);
+    });
+
+    let addToWishList = (itemId) => {
+        wishList.push(itemId);
+        localStorage.setItem(storageKey, JSON.stringify(wishList));
+    };
+
+    let removeFromWishList = (itemId) => {
+        for (let i = 0; i < wishList.length; i++) {
+            if (itemId === wishList[i]) {
+                wishList.splice(i,1);
+            }
+        }
+        localStorage.setItem(storageKey, JSON.stringify(wishList));
+    };
+
+    function loadWishList() {
+        let wishList = localStorage.getItem(storageKey);
+        if (!wishList) {
+            localStorage.setItem(storageKey, JSON.stringify([]));
+        } else {
+            wishList = JSON.parse(wishList);
+        }
+        return wishList;
+    }
 })(jQuery);
